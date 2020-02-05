cb3a868a493e8d69100436c516970da8ade04889
MHuiG <616001163@qq.com>
2020-02-05 16:05:06 +0800

    commit

--- GitPush.cmd
+++ GitPush.cmd
@@ -1,5 +1,5 @@
 git add -A &
 git commit -m "commit" &
-git remote set-url origin https://github.com/MHuiG/EnCpy3.7.6.git &
+git remote set-url origin https://github.com/MHuiG/EnCpy.git &
 git push -u origin master &
 pause
